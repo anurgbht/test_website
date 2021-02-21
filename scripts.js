61b4832b5ff7b5c337be9a8ff5807b6697c6bb7a
“anurgbht” <anuraag.bhatt@gmail.com>
2021-02-21 22:27:22 +0100

    included reference to D3.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ function getMachineInfo() {
     var fullVersion = '' + parseFloat(navigator.appVersion);
     var majorVersion = parseInt(navigator.appVersion, 10);
     var nameOffset, verOffset, ix;
-
+    
+    d3.select("header").style("color", "blue");
     alert('Hello\n' + nAgt);
 }
