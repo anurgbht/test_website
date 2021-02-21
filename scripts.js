4c7f7ef986cf177f3908cf9f7476b63b7329cc32
“anurgbht” <anuraag.bhatt@gmail.com>
2021-02-21 16:10:10 +0100

    Linked JS file to find system version.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function getMachineInfo() {
+    var nVer = navigator.appVersion;
+    var nAgt = navigator.userAgent;
+    var browserName = navigator.appName;
+    var fullVersion = '' + parseFloat(navigator.appVersion);
+    var majorVersion = parseInt(navigator.appVersion, 10);
+    var nameOffset, verOffset, ix;
+
+    alert('Hello\n' + nAgt);
+}
